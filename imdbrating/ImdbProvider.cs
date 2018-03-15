d0ae4c3ab15794f5227bf922d1e63915795b8f11
WilliamBZA <william.brander@gmail.com>
2018-03-15 17:15:00 +0200

    PUUUUSH

---
+++ imdbrating/ImdbProvider.cs
@@ -0,0 +1,33 @@
+﻿using IMDBCore;
+using ITOps.Composition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Threading.Tasks;
+
+namespace imdbrating
+{
+    public class ImdbProvider : IProvideData
+    {
+        public bool Matches(RouteData routeData, HttpRequest request)
+        {
+            var controller = ((string)routeData.Values["controller"]).ToLowerInvariant();
+            var action = ((string)routeData.Values["action"]).ToLowerInvariant();
+
+            return controller == "data" && request.Query.ContainsKey("moviedetails");
+        }
+
+        public async Task<string> GetImdbRating(string movieId)
+        {
+            var imdb = new Imdb("8c75101");
+            var movie = await imdb.GetMovieAsync(movieId);
+
+            return movie.ImdbRating;
+        }
+
+        public async Task PopulateData(dynamic viewModel, RouteData routeData, HttpRequest request)
+        {
+            viewModel.imdbRating = await GetImdbRating(request.Query["moviedetails"]);
+        }
+    }
+}
---
+++ userratings/UserRatingProvider.cs
@@ -0,0 +1,26 @@
+﻿using ITOps.Composition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Threading.Tasks;
+
+namespace userratings
+{
+    public class UserRatingProvider : IProvideData
+    {
+        public bool Matches(RouteData routeData, HttpRequest request)
+        {
+            var controller = ((string)routeData.Values["controller"]).ToLowerInvariant();
+            var action = ((string)routeData.Values["action"]).ToLowerInvariant();
+
+            return controller == "data" && request.Query.ContainsKey("moviedetails");
+        }
+
+        public Task PopulateData(dynamic viewModel, RouteData routeData, HttpRequest request)
+        {
+            viewModel.userRating = "1 / 10";
+
+            return Task.CompletedTask;
+        }
+    }
+}
